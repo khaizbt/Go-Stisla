7e25a10153b05081051082b21b68bb90655c82f6
Khaiz Badaru Tammam <tammam.dev@gmail.com>
2021-03-16 21:13:35 +0700

    init Project

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"goshop/middleware"
+	"goshop/repository"
+	"goshop/route"
+	"goshop/service"
+	"log"
+	"net/http"
+	"os"
+
+	sentrygin "github.com/getsentry/sentry-go/gin"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	// fmt.Println("masuk", os.Getenv("DB_USER"))
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_API"),
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+
+	userRepo := repository.NewUserRepository()
+	userService := service.NewUserService(userRepo)
+
+	secureMiddleware := middleware.SecureMiddleware()
+
+	router := gin.Default()
+	//router.LoadHTMLGlob("web/assets/*")
+	router.Static("/assets", "./web/assets")
+	router.Use(secureMiddleware)
+	router.Use(sentrygin.New(sentrygin.Options{}))
+	route.RouteUser(router, userService)
+	router.LoadHTMLGlob("web/base/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "header.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8000")
+}
